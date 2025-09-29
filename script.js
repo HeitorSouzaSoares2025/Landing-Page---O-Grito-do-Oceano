6c9a46cea28c6d93860975c791cb1090e0a300f4
HeitorSouza_Soares <heitorinformaticasala09@gmail.com>
2025-09-28 23:13:26 -0300

    Delete script.js

--- script.js
+++
@@ -1,6 +0,0 @@
-// Cursor customizado
-const cursor = document.getElementById("custom-cursor");
-document.addEventListener("mousemove", e => {
-  cursor.style.top = e.clientY + "px";
-  cursor.style.left = e.clientX + "px";
-});
